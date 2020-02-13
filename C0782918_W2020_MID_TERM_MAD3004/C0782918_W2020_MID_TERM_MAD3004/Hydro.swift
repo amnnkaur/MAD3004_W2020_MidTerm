b85f511f6dafff4a2afed4a4d8d0a5269b9ac7ce
Aman Kaur <amankaur2628@gmail.com>
2020-02-13 18:34:06 -0500

    [Aman] Class Hydro: Created and variables added

---
+++ C0782918_W2020_MID_TERM_MAD3004/C0782918_W2020_MID_TERM_MAD3004/Hydro.swift
@@ -0,0 +1,15 @@
+//
+//  Hydro.swift
+//  C0782918_W2020_MID_TERM_MAD3004
+//
+//  Created by MacStudent on 2020-02-13.
+//  Copyright © 2020 MacStudent. All rights reserved.
+//
+
+import Foundation
+
+public class Hydro : Bill
+{
+    var agencyName : String
+    var unitConsumed : String
+}
