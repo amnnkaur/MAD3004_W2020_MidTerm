225979bca4187fc884bb630290907ff8ced18e27
Aman Kaur <amankaur2628@gmail.com>
2020-02-13 18:07:46 -0500

    [Aman]Class Bill: Created

---
+++ C0782918_W2020_MID_TERM_MAD3004/C0782918_W2020_MID_TERM_MAD3004/Bill.swift
@@ -0,0 +1,14 @@
+//
+//  Bill.swift
+//  C0782918_W2020_MID_TERM_MAD3004
+//
+//  Created by MacStudent on 2020-02-13.
+//  Copyright © 2020 MacStudent. All rights reserved.
+//
+
+import Foundation
+
+public class Bill {
+    
+}
+
