7f1132746c4c7a48c9cb74e4e3b37c6ac2f273bb
Amanpreet kaur <amankaur2628@gmail.com>
2020-02-18 17:12:39 -0500

    [Aman] Bill assigned to customers

--- C0782918_W2020_MID_TERM_MAD3004/C0782918_W2020_MID_TERM_MAD3004/main.swift
+++ C0782918_W2020_MID_TERM_MAD3004/C0782918_W2020_MID_TERM_MAD3004/main.swift
@@ -13,7 +13,13 @@ var bill02 = Mobile(billId: 2, billDate: "10-04-2019", billType: Enum.type.MOBIL
 var bill03 = Hydro(billId: 3, billDate: "13-06-2019", billType: Enum.type.HYDRO, totalBillAmount: 150, agencyName: "Ontario electricity Services Inc.", unitConsumed: 210)
 
 
-var cust1 = Customer(customerId: 1001, firstName: "Monika", lastName: "Sharma", emailId: "ms@gmail.com")
-var cust2 = Customer(customerId: 1002, firstName: "IKROOP", lastName: "virk", emailId: "ikroop@gmail.com")
-var cust3 = Customer(customerId: 1003, firstName: "Aman", lastName: "Kaur", emailId: "aman@gmail.com")
+var cust1 = Customer(customerId: 01, firstName: "Aman", lastName: "Kaur", emailId: "amankaur@gmail.com")
+var cust2 = Customer(customerId: 02, firstName: "Anmol", lastName: "singh", emailId: "anmol_singh@gmail.com")
+var cust3 = Customer(customerId: 03, firstName: "Monika", lastName: "Sharma", emailId: "monika123@gmail.com")
 
+
+cust1.addBillToCustomer(bill: bill02)
+cust1.addBillToCustomer(bill: bill03)
+cust2.addBillToCustomer(bill: bill01)
+cust2.addBillToCustomer(bill: bill02)
+cust3.addBillToCustomer(bill: bill03)
