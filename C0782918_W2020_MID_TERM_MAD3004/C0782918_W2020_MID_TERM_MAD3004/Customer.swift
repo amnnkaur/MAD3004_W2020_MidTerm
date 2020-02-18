4d6092eb4da1c4c77a0c6c1029e78f35970d9f00
Aman Kaur <amankaur2628@gmail.com>
2020-02-18 17:57:29 -0500

    [Aman] Email Validation func added

--- C0782918_W2020_MID_TERM_MAD3004/C0782918_W2020_MID_TERM_MAD3004/Customer.swift
+++ C0782918_W2020_MID_TERM_MAD3004/C0782918_W2020_MID_TERM_MAD3004/Customer.swift
@@ -26,6 +26,13 @@ public class Customer
         
     }
     
+    func emailValidation(email: String) -> Bool {
+        let emailFormat = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+
+        let emailPredicate = NSPredicate(format:"SELF MATCHES %@", emailFormat)
+        return emailPredicate.evaluate(with: email)
+    }
+    
     func addBillToCustomer(bill:Bill)
     {
            billDictionary.updateValue(bill, forKey: bill.billId)
