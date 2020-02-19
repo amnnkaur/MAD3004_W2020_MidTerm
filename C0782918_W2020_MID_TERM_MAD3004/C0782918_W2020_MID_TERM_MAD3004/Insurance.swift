0bc4bc42b903a089df14de6b3b41e7327f8b8624
Aman Kaur <amankaur2628@gmail.com>
2020-02-18 19:10:26 -0500

    [Aman] Class Insurance and variables created

---
+++ C0782918_W2020_MID_TERM_MAD3004/C0782918_W2020_MID_TERM_MAD3004/Insurance.swift
@@ -0,0 +1,17 @@
+//
+//  Insurance.swift
+//  C0782918_W2020_MID_TERM_MAD3004
+//
+//  Created by MacStudent on 2020-02-18.
+//  Copyright © 2020 MacStudent. All rights reserved.
+//
+
+import Foundation
+
+public class Insurance : Bill
+{
+  var provider : String
+  var insuranceType : String
+  var startDate = String()
+  var endDate = String()
+}
